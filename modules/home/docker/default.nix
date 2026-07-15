8cbc80e1a14a4ff6b12aa12000de1adb23f01a8e
maulanasdqn <maulanasdqn@gmail.com>
2026-07-15 14:04:46 +0700

    feat(docker): size colima vm (6 cpu / 12 gib) via launchd agent + colima-up alias

--- modules/home/docker/default.nix
+++ modules/home/docker/default.nix
@@ -1,4 +1,13 @@
 { pkgs, username, ... }:
+let
+  colimaCpu = "6";
+  colimaMemory = "12";
+  colimaDisk = "100";
+  colimaStart = pkgs.writeShellScript "colima-autostart" ''
+    export PATH=${pkgs.colima}/bin:/usr/bin:/bin:/usr/sbin:/sbin
+    colima start --cpu ${colimaCpu} --memory ${colimaMemory} --disk ${colimaDisk} || true
+  '';
+in
 {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
@@ -7,6 +16,17 @@
       docker-compose
     ];
 
+    launchd.agents.colima = {
+      enable = true;
+      config = {
+        ProgramArguments = [ "${colimaStart}" ];
+        RunAtLoad = true;
+        KeepAlive = false;
+        StandardOutPath = "/tmp/colima.autostart.out.log";
+        StandardErrorPath = "/tmp/colima.autostart.err.log";
+      };
+    };
+
     programs.zsh.shellAliases = {
       dc = "docker compose";
       dps = "docker ps";
@@ -14,6 +34,7 @@
       di = "docker images";
       dex = "docker exec -it";
       dlog = "docker logs -f";
+      colima-up = "colima start --cpu ${colimaCpu} --memory ${colimaMemory} --disk ${colimaDisk}";
     };
   };
 }
